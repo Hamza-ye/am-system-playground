c2437dc7a139b9c1f9cbe54a82f1a22e1b199b39
Hamza-ye <7amza.it@gmail.com>
2021-12-05 03:45:32 +0300

    Add temps
    Signed-off-by: Hamza-ye <7amza.it@gmail.com>

--- src/main/java/org/nmcpye/activitiesmanagement/extended/hibernatemodule/hibernate/HibernateMetadata.java
+++ src/main/java/org/nmcpye/activitiesmanagement/extended/hibernatemodule/hibernate/HibernateMetadata.java
@@ -5,20 +5,21 @@ import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.boot.spi.SessionFactoryBuilderFactory;
 import org.hibernate.boot.spi.SessionFactoryBuilderImplementor;
 
-public class HibernateMetadata implements SessionFactoryBuilderFactory
-{
+public class HibernateMetadata implements SessionFactoryBuilderFactory {
     private static final ThreadLocal<MetadataImplementor> metadataImplementor = new ThreadLocal<>();
+//    private static MetadataImplementor metadataImplementor;
 
     @Override
     public SessionFactoryBuilder getSessionFactoryBuilder(MetadataImplementor metadataImplementor,
-                                                          SessionFactoryBuilderImplementor defaultBuilder )
-    {
-        HibernateMetadata.metadataImplementor.set( metadataImplementor );
+                                                          SessionFactoryBuilderImplementor defaultBuilder) {
+        HibernateMetadata.metadataImplementor.set(metadataImplementor);
+//        HibernateMetadata.metadataImplementor= metadataImplementor;
         return defaultBuilder;
     }
 
-    public static MetadataImplementor getMetadataImplementor()
-    {
+    public static MetadataImplementor getMetadataImplementor() {
+
         return metadataImplementor.get();
+//        return metadataImplementor;
     }
 }
